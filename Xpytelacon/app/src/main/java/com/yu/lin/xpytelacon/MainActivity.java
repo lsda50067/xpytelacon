ce11b510356276b204b317ac4346ed5aa4509f0b
Yu <lsda10108017@gmail.com>
2017-10-28 18:28:35 +0800

    remove object to other  class

--- Xpytelacon/app/src/main/java/com/yu/lin/xpytelacon/MainActivity.java
+++ Xpytelacon/app/src/main/java/com/yu/lin/xpytelacon/MainActivity.java
@@ -29,6 +29,7 @@ import android.widget.TextView;
 import android.support.design.widget.NavigationView;
 
 import com.yu.lin.xpytelacon.adapter.CustomAdapter;
+import com.yu.lin.xpytelacon.provider.Images;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -44,6 +45,9 @@ import java.util.Map;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener,ListView.OnItemClickListener, NavigationView.OnNavigationItemSelectedListener{
 
     private static final String TAG = MainActivity.class.getName();
+
+    private Integer[] mImages;
+    private String[] mDate;
     private ListView mListView;
     private CustomAdapter mCustomAdapter;
 //    private LruCache<String, Bitmap> mLruCache;
@@ -70,7 +74,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onResume() {
         super.onResume();
+        // setData
+        serData();
+        // setListener
         setListener();
+        // setAdapter
         setAdapter();
     }
 
@@ -95,6 +103,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         navigationView = (NavigationView) findViewById(R.id.nav_view);
     }
 
+    private void serData() {
+        mImages = Images.images;
+        mDate = Images.date;
+    }
+
     private void reSetToolBar(){
         setSupportActionBar(toolbar);
     }
@@ -113,7 +126,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void setAdapter() {
-        mCustomAdapter = new CustomAdapter(getApplicationContext(), images, date);
+        mCustomAdapter = new CustomAdapter(getApplicationContext(), mImages, mDate);
         mListView.setAdapter(mCustomAdapter);
     }
 
@@ -165,66 +178,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
-
-    // image source form https://newevolutiondesigns.com/50-fresh-hd-wallpapers // no use business
-    private Integer[] images = {
-            R.drawable.hd_wallpaper_preview_1,
-            R.drawable.hd_wallpaper_preview_2,
-            R.drawable.hd_wallpaper_preview_3,
-            R.drawable.hd_wallpaper_preview_4,
-            R.drawable.hd_wallpaper_preview_5,
-            R.drawable.hd_wallpaper_preview_6,
-            R.drawable.hd_wallpaper_preview_7,
-            R.drawable.hd_wallpaper_preview_8,
-            R.drawable.hd_wallpaper_preview_9,
-            R.drawable.hd_wallpaper_preview_10,
-            R.drawable.hd_wallpaper_preview_11,
-            R.drawable.hd_wallpaper_preview_12,
-            R.drawable.hd_wallpaper_preview_13,
-            R.drawable.hd_wallpaper_preview_14,
-            R.drawable.hd_wallpaper_preview_15,
-            R.drawable.hd_wallpaper_preview_16,
-            R.drawable.hd_wallpaper_preview_17,
-            R.drawable.hd_wallpaper_preview_18,
-            R.drawable.hd_wallpaper_preview_19,
-            R.drawable.hd_wallpaper_preview_20,
-            R.drawable.hd_wallpaper_preview_21,
-            R.drawable.hd_wallpaper_preview_22,
-            R.drawable.hd_wallpaper_preview_23,
-            R.drawable.hd_wallpaper_preview_24,
-            R.drawable.hd_wallpaper_preview_25,
-            R.drawable.hd_wallpaper_preview_26,
-            R.drawable.hd_wallpaper_preview_27,
-            R.drawable.hd_wallpaper_preview_28,
-            R.drawable.hd_wallpaper_preview_29,
-            R.drawable.hd_wallpaper_preview_30,
-            R.drawable.hd_wallpaper_preview_31,
-            R.drawable.hd_wallpaper_preview_32,
-            R.drawable.hd_wallpaper_preview_33,
-            R.drawable.hd_wallpaper_preview_34,
-            R.drawable.hd_wallpaper_preview_35,
-            R.drawable.hd_wallpaper_preview_36,
-            R.drawable.hd_wallpaper_preview_37,
-            R.drawable.hd_wallpaper_preview_38,
-            R.drawable.hd_wallpaper_preview_39,
-            R.drawable.hd_wallpaper_preview_40,
-            R.drawable.hd_wallpaper_preview_41,
-            R.drawable.hd_wallpaper_preview_42,
-            R.drawable.hd_wallpaper_preview_43,
-            R.drawable.hd_wallpaper_preview_44,
-            R.drawable.hd_wallpaper_preview_45,
-            R.drawable.hd_wallpaper_preview_47,
-            R.drawable.hd_wallpaper_preview_48,
-            R.drawable.hd_wallpaper_preview_49,
-            R.drawable.hd_wallpaper_preview_50
-    };
-
-    private String[] date = {
-            "1","2","3","4","5","6","7","8","9","10",
-            "11","12","13","14","15","16","17","18","19","20",
-            "21","22","23","24","25","26","27","28","29","30",
-            "31","32","33","34","35","36","37","38","39","40",
-            "41","42","43","44","45","47","48","49","50"
-    };
 
 }
---
+++ Xpytelacon/app/src/main/java/com/yu/lin/xpytelacon/provider/Images.java
@@ -0,0 +1,72 @@
+package com.yu.lin.xpytelacon.provider;
+
+import com.yu.lin.xpytelacon.R;
+
+/**
+ * Created by Lin-Yu on 27/10/2017.
+ *
+ */
+
+public class Images {
+
+    // image source form https://newevolutiondesigns.com/50-fresh-hd-wallpapers // no use business
+    public static final Integer[] images = {
+            R.drawable.hd_wallpaper_preview_1,
+            R.drawable.hd_wallpaper_preview_2,
+            R.drawable.hd_wallpaper_preview_3,
+            R.drawable.hd_wallpaper_preview_4,
+            R.drawable.hd_wallpaper_preview_5,
+            R.drawable.hd_wallpaper_preview_6,
+            R.drawable.hd_wallpaper_preview_7,
+            R.drawable.hd_wallpaper_preview_8,
+            R.drawable.hd_wallpaper_preview_9,
+            R.drawable.hd_wallpaper_preview_10,
+            R.drawable.hd_wallpaper_preview_11,
+            R.drawable.hd_wallpaper_preview_12,
+            R.drawable.hd_wallpaper_preview_13,
+            R.drawable.hd_wallpaper_preview_14,
+            R.drawable.hd_wallpaper_preview_15,
+            R.drawable.hd_wallpaper_preview_16,
+            R.drawable.hd_wallpaper_preview_17,
+            R.drawable.hd_wallpaper_preview_18,
+            R.drawable.hd_wallpaper_preview_19,
+            R.drawable.hd_wallpaper_preview_20,
+            R.drawable.hd_wallpaper_preview_21,
+            R.drawable.hd_wallpaper_preview_22,
+            R.drawable.hd_wallpaper_preview_23,
+            R.drawable.hd_wallpaper_preview_24,
+            R.drawable.hd_wallpaper_preview_25,
+            R.drawable.hd_wallpaper_preview_26,
+            R.drawable.hd_wallpaper_preview_27,
+            R.drawable.hd_wallpaper_preview_28,
+            R.drawable.hd_wallpaper_preview_29,
+            R.drawable.hd_wallpaper_preview_30,
+            R.drawable.hd_wallpaper_preview_31,
+            R.drawable.hd_wallpaper_preview_32,
+            R.drawable.hd_wallpaper_preview_33,
+            R.drawable.hd_wallpaper_preview_34,
+            R.drawable.hd_wallpaper_preview_35,
+            R.drawable.hd_wallpaper_preview_36,
+            R.drawable.hd_wallpaper_preview_37,
+            R.drawable.hd_wallpaper_preview_38,
+            R.drawable.hd_wallpaper_preview_39,
+            R.drawable.hd_wallpaper_preview_40,
+            R.drawable.hd_wallpaper_preview_41,
+            R.drawable.hd_wallpaper_preview_42,
+            R.drawable.hd_wallpaper_preview_43,
+            R.drawable.hd_wallpaper_preview_44,
+            R.drawable.hd_wallpaper_preview_45,
+            R.drawable.hd_wallpaper_preview_47,
+            R.drawable.hd_wallpaper_preview_48,
+            R.drawable.hd_wallpaper_preview_49,
+            R.drawable.hd_wallpaper_preview_50
+    };
+
+    public static final String[] date = {
+            "1","2","3","4","5","6","7","8","9","10",
+            "11","12","13","14","15","16","17","18","19","20",
+            "21","22","23","24","25","26","27","28","29","30",
+            "31","32","33","34","35","36","37","38","39","40",
+            "41","42","43","44","45","47","48","49","50"
+    };
+}
